3d35c86144857b382a5ddf6be053a544ebf555fb
cj-smithy <chris.smith@red-gate.com>
2016-03-21 16:39:43 +0000

    try a simple commit.

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
 
 namespace TheWorld
 {
@@ -14,8 +15,7 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        { }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
