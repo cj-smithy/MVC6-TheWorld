5efb11fb53ca9fcb68cc56fffb7e98d6e43ed499
cj-smithy <chris.smith@red-gate.com>
2016-04-04 17:41:44 +0100

    Learning about javascript global scope and how to use Bower for client side dependencies.

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,3 +1,17 @@
 ﻿// site.js
-var element = document.getElementById("username");
-element.innerHTML = "Mr " + element.innerHTML;
+
+// define and execute an anonymous function to avoid names leaking into global scope
+(function() {
+    var element = document.getElementById("username");
+    element.innerHTML = "Mr " + element.innerHTML;
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function() {
+        main.style["background-color"] = "#888";
+    }
+    main.onmouseleave = function() {
+        // set this style to be empty, so the rule in css will be used
+        main.style["background-color"] = "";
+    }
+})();
+
