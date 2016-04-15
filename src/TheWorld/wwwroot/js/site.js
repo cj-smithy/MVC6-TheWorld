df8ad5a53cda4b76af5551505c1147fd6a904a88
cj-smithy <chris.smith@red-gate.com>
2016-04-15 16:17:10 +0100

    Using jQery for the first time!

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -2,16 +2,26 @@
 
 // define and execute an anonymous function to avoid names leaking into global scope
 (function() {
-    var element = document.getElementById("username");
-    element.innerHTML = "Mr " + element.innerHTML;
+    // can use "$" instead of "jQuery"
+    var element = jQuery("#username");
+    element.text("Something else");
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function() {
-        main.style["background-color"] = "#888";
-    }
-    main.onmouseleave = function() {
+    var main = jQuery("#main");
+    main.on("mouseenter", function() {
+        main.style = "background-color: #888;";
+    });
+
+    main.on("mouseleave", function() {
         // set this style to be empty, so the rule in css will be used
         main.style["background-color"] = "";
-    }
+    });
+
+    var menuItems = $("ul.menu li a");
+    // handle events on all menu items with one callback!
+    menuItems.on("click", function () {
+        //get hold of the object (using this)
+        var textToShow = $(this);
+        alert("hello "+ textToShow.text());
+    });
 })();
 
